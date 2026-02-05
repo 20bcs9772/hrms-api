fdf77885aa8ba07ef795347b8a65f02461d4f390
Madhav-Bansal-Web <madhav@oceaniek.org>
2026-02-05 16:44:52 +0530

    updated database url

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:madhav@localhost:5432/hrms"
+    "postgresql://12c23cc4b7faaa09ecb7a9e2680d33c763926f5fc77bda20e73770d856f513fd:sk__uiYDMUda4dN7_qb4tWvk@db.prisma.io:5432/postgres"
 )
 
 engine = create_engine(DATABASE_URL)
